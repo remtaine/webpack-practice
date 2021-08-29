56f98420d6d2277b2f860032599062e182b4abf7
Benjo Kho <benjokho@gmail.com>
2021-08-29 15:21:32 +0800

    Add imports

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
+import "./style.scss";
+import _ from 'lodash';
+
 function component() {
     const element = document.createElement('div');
 
-    // Lodash, currently included via a script, is required for this line to work
+    // lodash now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
     return element;
 }
 
 document.body.appendChild(component());
+
+console.log("Follow God")
